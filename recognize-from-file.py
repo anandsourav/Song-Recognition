ed45462b7f2dcf4fad192a8c8c8ba67c6c863479
Sourav Anand <73410567+anandsourav@users.noreply.github.com>
2021-04-06 15:33:42 +0530

    Add files via upload

---
+++ recognize-from-file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# from libs.reader_file import FileReader
+
+if __name__ == '__main__':
+    song = None
+    seconds = 5
+
+    # r = FileReader(123)
+    # r.recognize(seconds=seconds)
+    #
+    # print(song)
